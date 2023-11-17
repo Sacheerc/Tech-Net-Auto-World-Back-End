3a58c54f21d1b7de916c9c4f14b6f553d3aa2aa6
Sachintha Rathnayake <madushansachintha@gmail.com>
2023-11-17 20:40:06 +0530

    Initial commit

---
+++ src/test/java/com/technet/api/TechNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technet.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
